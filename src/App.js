503bd1d27d0445dedf597a85f4330305c6abe280
amirhyder <45149145+amirhyder@users.noreply.github.com>
2021-09-14 11:51:22 +0500

    Assignment 4 done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Blog from './containers/Blog/Blog';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Blog />
+    </div>
+  );
+
+}
+
+export default App;
